fe5f36ac683e8fa82921fa9e83988b634b8022e5
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-02-14 22:18:49 +0200

    Write contributions difference to the log file

--- contribution-spy.py
+++ contribution-spy.py
@@ -31,6 +31,8 @@ def main():
     if len(username) == 0:
         return
 
+    contribs_prev = None
+
     while True:
         # Get number of contributions
         r = get(URL % username)
@@ -38,21 +40,32 @@ def main():
         parser.feed(r.text)
         d = dict(parser.rects[-1])
 
-        number = d["data-count"]
+        number = int(d["data-count"])
         date = d["data-date"]
 
-        current_date = datetime.datetime.now()
+        # If this is the 1st loop, set the previous contributions to current
+        if contribs_prev is None:
+            contribs_prev = number
 
-        # If log file does not exist, write headers to it
+        # Get current time and difference of contributions
+        current_date = datetime.datetime.now()
+        contribs_diff = number - contribs_prev
+
+        # If log file does not exist, create it with headers
         if not os.path.isfile(log_file):
             # Create log file with headers
             with open(log_file, "w") as f:
-                f.write("curr_time,ghb_date,contribs\n")
+                f.write("curr_time,ghb_date,contribs,diff\n")
 
         # Write to log file
         with open(log_file, "a") as f:
-            f.write(str(current_date) + "," + date + "," + str(number) + "\n")
-            # print(str(current_date) + "," + date + "," + str(number) + "\n")
+            f.write(str(current_date) + "," + date + "," + str(number) +
+                    "," + str(contribs_diff) + "\n")
+            # print(str(current_date) + "," + date + "," + str(number) +
+            #       "," + str(contribs_diff) + "\n")
+
+        # Save new "previous" number of contributions
+        contribs_prev = number
 
         # Wait five minutes
         time.sleep(5)
